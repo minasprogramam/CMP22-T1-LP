956cae4acd1ed190f59cb030776171def1aa9852
Lisandra Cruz <lisansouza@gmail.com>
2022-05-27 10:58:11 -0300

    Exercício de calculo da circunferencia

---
+++ LP1/codigo-aula/26-05/exercicio/operadores-aritmetricos/circunferencia/circunferencia.js
@@ -0,0 +1,17 @@
+/* 
+  Crie um programa para medir o 
+  comprimento da circunferencia
+  
+  # formula -> comprimento = 2 x PI x Raio 
+  considere PI = 3.1415
+*/
+
+const pi = Math.PI;
+const raio = 2;
+const comprimento = 2 * pi * raio;
+
+console.log(`O circulo tem raio ${raio} e seu comprimento é ${comprimento}`);
+
+const frase1 = "ola mundo";
+const frase2 = "hello word";
+console.log(frase1 === frase1);
