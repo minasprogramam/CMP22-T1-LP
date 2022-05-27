d4e4c998ac8023ce2defd96ab98990acac680179
Lisandra Cruz <lisansouza@gmail.com>
2022-05-27 10:55:28 -0300

    Codigo de aula sobre concatenação

---
+++ LP1/codigo-aula/26-05/aula/index.js
@@ -0,0 +1,21 @@
+/*
+  Concatenar é o ato de juntar textos 
+  e/ou variáveis em uma unica string.
+*/
+const primeiroNome = "Lisandra";
+const ultimoNome = "Cruz";
+const idade = 27;
+
+console.log(
+  `Meu nome é ${primeiroNome} ${ultimoNome} e eu tenho ${idade} anos`
+);
+
+console.log(
+  "Meu nome é " +
+    primeiroNome +
+    " " +
+    ultimoNome +
+    " e eu tenho " +
+    idade +
+    " anos"
+);
