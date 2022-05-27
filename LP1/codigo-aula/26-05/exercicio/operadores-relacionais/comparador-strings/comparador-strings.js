70c0252e8a2680914db280d73fdad876827e0503
Lisandra Cruz <lisansouza@gmail.com>
2022-05-27 10:59:20 -0300

    Exercício de operadores relacionais para comparar strings

---
+++ LP1/codigo-aula/26-05/exercicio/operadores-relacionais/comparador-strings/comparador-strings.js
@@ -0,0 +1,9 @@
+/* 
+  Faça um programa que armazene duas frases e 
+  compare se elas são iguais.
+*/
+
+const frase1 = "ola mundo";
+const frase2 = "hello word";
+
+console.log(frase1 === frase1);
