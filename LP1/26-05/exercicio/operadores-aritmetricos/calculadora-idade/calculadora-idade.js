05060df74618b9b91de995e30088e76e2c42986b
Lisandra Cruz <lisansouza@gmail.com>
2022-06-02 11:30:26 -0300

    Reorganização de pastas

---
+++ LP1/26-05/exercicio/operadores-aritmetricos/calculadora-idade/calculadora-idade.js
@@ -0,0 +1,5 @@
+/* 
+  Faça um programa que obtenha o ano de 
+  nascimento da usuária e retorne 
+  quantos anos ela tem.
+*/
---
+++ LP1/26-05/exercicio/operadores-relacionais/comparador-numeros/comparador-numeros.js
@@ -0,0 +1,5 @@
+/* 
+  Faça um programa que armazene dois 
+  números e retorne se o primeiro 
+  é maior que o segundo número.
+*/
